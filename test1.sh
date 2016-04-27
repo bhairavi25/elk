742097d7ea5952a2171420bc8f539857a8b9dadf
Bhairavi Sankar <bhairavi.sankar@gmail.com>
2016-04-27 09:44:09 -0500

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -4,6 +4,7 @@ echo \"Hello world1\"$Server_name >> /root/testfile.txt
 echo $SALT_API_USER >> /root/testfile.txt
 echo $SALT_API_PASS >> /root/testfile.txt
 echo $test_text1 >> /root/testfile.txt
-echo $ip >> /root/testfile.txt
-echo $mac_addr >> /root/testfile.txt
+echo $test_text2 >> /root/testfile.txt
+#echo $ip >> /root/testfile.txt
+#echo $mac_addr >> /root/testfile.txt
 echo \"last line\" >> /root/testfile.txt
